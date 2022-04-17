921946b3b0d71a02b3d66a77be35533493d6e149
PRASAD <PRASAD@LAPTOP-L7JLSIFJ>
2022-04-17 15:16:39 +0530

    first commit

---
+++ Selenium/src/Pilot.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Pilot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chromedriver.exe", "/Users/PRASAD/eclipse-workspace/Selenium/Chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.freeCodeCamp.org");
+		System.out.println(driver.getTitle());
+		
+		
+	}
+
+}
