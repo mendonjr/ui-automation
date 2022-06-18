740b66bc9f670ea3b6e907ea8e89d1f5dcc67ec1
PrasadMendon <prasadmendon744@gmail.com>
2022-06-18 17:10:28 +0530

    june 18

---
+++ Selenium/src/Practice/Assignment2.java
@@ -0,0 +1,46 @@
+package Practice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "/Users/PRASAD/eclipse-workspace/Selenium/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();	
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		
+		WebElement ddElement = driver.findElement(By.id("exampleFormControlSelect1"));
+		Select dropdown = new Select(ddElement);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		
+		
+			driver.findElement(By.xpath("(//input[@name='name'])[1]")).sendKeys("Prasad Mendon");
+			driver.findElement(By.xpath("//input[@name='email']")).sendKeys("wdw@wd.com");
+			driver.findElement(By.xpath("//input[@type='password']")).sendKeys("ola@amigos");
+			driver.findElement(By.xpath("//input[@type='checkbox']")).click();
+			
+			dropdown.selectByIndex(1);
+			driver.findElement(By.xpath("//label[text()='Student']")).click();
+			driver.findElement(By.xpath("//label[text()='Date of Birth']/following-sibling::input")).sendKeys("01-02-1964");
+			
+			driver.findElement(By.xpath("//input[@type='submit']")).click();
+			boolean isVisible = driver.findElement(By.xpath("//a[@class='close']/parent::div[1]")).isDisplayed();
+			
+			Assert.assertTrue(isVisible);
+			
+			System.out.println("Test Case Passed");
+			
+			
+			
+	}
+
+}
--- Selenium/src/Practice/UpdatedDropdown.java
+++ Selenium/src/Practice/UpdatedDropdown.java
@@ -1,8 +1,12 @@
 package Practice;
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 public class UpdatedDropdown {
 
@@ -11,12 +15,40 @@ public class UpdatedDropdown {
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/PRASAD/eclipse-workspace/Selenium/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();	
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-		Select dropdown=new Select(driver.findElement(By.xpath(".//*[@id='ctl00_mainContent_ddl_Adult']")));
+		WebElement dd = driver.findElement(By.xpath("//*[@id='ctl00_mainContent_ddl_Adult']"));
+		Select dropdown=new Select(dd);
 
 		dropdown.selectByIndex(4);
 
-		dropdown.selectByVisibleText("9 Adults");
+		//dropdown.selectByVisibleText("9 Adults");
+		
+		System.out.println(driver.findElement(By.name("ctl00$mainContent$view_date2")).isEnabled());
+
+		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+
+		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+
+		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+
+		if(driver.findElement(By.id("Div1")).getAttribute("style").contains("1"))
+
+		{
+
+		System.out.println("its enabled");
+
+		Assert.assertTrue(true);
+
+		}
+
+		else
+
+		{
+
+		Assert.assertTrue(false);
+
+		}
 
 		
 	}
